cf3ecb3b0e67ccde224930b73500e969ceb99979
Armando Gonzalez <agonzalezsosto@gmail.com>
2021-04-07 10:55:54 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,16 @@ router.get('/', (req, res) => {
 
 const app = express()
 app.use(router)
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
 const port = 8080
 
 const http = createServer(app)
